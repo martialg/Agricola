ede063481baa721e8ab9136fb0ec7f93ccd306b3
Quentin Burtschell <quentin.burtschell@gmail.com>
2013-05-01 17:40:31 +0200

    fix

--- src/ressources/Ressource.java
+++ src/ressources/Ressource.java
@@ -60,17 +60,7 @@ public class Ressource {
         return this.sanglier;
     }
        
-    public Legume getLegume(){
-        return this.legume;
-    }
     
-    public Cereale getCereale(){
-        return this.cereale;
-    }
-    
-    public Pierre getPierre(){
-        return this.pierre;
-    }
 
     public Ressource(){
         this.nb_membres_famille = 3; //3 car 2 utilisés à l'initialisation
