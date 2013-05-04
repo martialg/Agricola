4b30e438ff4ce1fd363cb57cb7dbe2dc90d53009
Quentin Burtschell <quentin.burtschell@gmail.com>
2013-05-04 16:36:03 +0200

    1er page d'accueil

--- src/interfaceGraphique/Accueil.java
+++ src/interfaceGraphique/Accueil.java
@@ -1,130 +1,193 @@
 package interfaceGraphique;
 
-import java.awt.Button;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.font.TextAttribute;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
-public class Accueil extends JPanel implements ActionListener{
+public class Accueil extends JPanel implements ActionListener {
 
     private static JFrame fenetre;
-    private JButton bouton_jouer;
-    private static JoliBouton bouton_quitter;
-    private static boolean fin;
-    private BufferedImage background;
+    private static BufferedImage background;
     private static Accueil accueil;
-    
+    private static JoliBouton bouton_fermer;
+    private static JoliBouton bouton_jouer;
+    private static JoliBouton bouton_regle;
+    private static JLabel label_choix_joueur;
 
-    public Accueil(BufferedImage background){
-        this.background = background;
-    }
     
-    public static void start() throws IOException {
+    public Accueil() {
         try {
-            UIManager.setLookAndFeel( UIManager.getCrossPlatformLookAndFeelClassName());
-            fin = false;
-            Accueil.affichage();
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Accueil.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            Logger.getLogger(Accueil.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            Logger.getLogger(Accueil.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (UnsupportedLookAndFeelException ex) {
+            background = ImageIO.read(new File("images/background2.png"));
+        } catch (IOException ex) {
             Logger.getLogger(Accueil.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-    }
-
-    private static void affichage() throws IOException {
-        BufferedImage background = ImageIO.read(new File("images/background2.png"));
-        accueil = new Accueil(background);
-        accueil.setOpaque(true);
-        accueil.setLayout(null);
-        
-
-        //quitter
-        
-        bouton_quitter = new JoliBouton("Fermer");
-        /*Image img_normal = ImageIO.read(new File("images/button.png"));
-        bouton_quitter.setIcon(new ImageIcon(img_normal));
-        bouton_quitter.setText("Quitter");
-        bouton_quitter.addActionListener(accueil);
-        bouton_quitter.setFont(new Font("Matura MT Script Capitals", Font.ROMAN_BASELINE, 20));
-        
-        bouton_quitter.setHorizontalTextPosition(JButton.CENTER);
-        bouton_quitter.setVerticalTextPosition(JButton.CENTER);
-        Color c = new Color(0x643E1D);
-        bouton_quitter.setForeground(c);
-        bouton_quitter.setFocusPainted(false);
-        bouton_quitter.setBorderPainted(false);
-        bouton_quitter.setContentAreaFilled(false);
-        
-        bouton_quitter.addMouseListener(new java.awt.event.MouseAdapter() {
-            Image img_normal = ImageIO.read(new File("images/button.png"));
-            Image img_over = ImageIO.read(new File("images/button-over.png"));
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                bouton_quitter.setIcon(new ImageIcon(img_over));
-                Color c = new Color(0xDA6C0D);
-                bouton_quitter.setForeground(c);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                bouton_quitter.setIcon(new ImageIcon(img_normal));
-                Color c = new Color(0x643E1D);
-                bouton_quitter.setForeground(c);
-            }
-        });*/
-        
-        
-        bouton_quitter.setBounds(530, 5, 123, 41);
-        
-        bouton_quitter.addActionListener(accueil);
-        accueil.add(bouton_quitter);
-        
-        
-        
         fenetre = new JFrame();
-        JFrame.setDefaultLookAndFeelDecorated(false);
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fenetre.setContentPane(accueil);
-        fenetre.setSize(660,420);
+        fenetre.setSize(660, 420);
+        fenetre.setContentPane(this);
         Color c1 = new Color(0xC96D1C);
         fenetre.getContentPane().setBackground(c1);
         fenetre.setResizable(false);
         fenetre.setLocationRelativeTo(fenetre.getParent());
         fenetre.setVisible(true);
     }
-    
+
+    public static void start() throws IOException {
+        Accueil.affichage();
+    }
+
+    private static void affichage() throws IOException {
+
+        //bouton fermer
+        bouton_fermer = new JoliBouton("Fermer");
+        bouton_fermer.createButton();
+        bouton_fermer.addMouseListener(new java.awt.event.MouseAdapter() {
+            Image img_normal = ImageIO.read(new File("images/button.png"));
+            Image img_over = ImageIO.read(new File("images/button-over.png"));
+
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                bouton_fermer.setIcon(new ImageIcon(img_over));
+                Color c = new Color(0xDA6C0D);
+                bouton_fermer.setForeground(c);
+            }
+
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                bouton_fermer.setIcon(new ImageIcon(img_normal));
+                Color c = new Color(0x643E1D);
+                bouton_fermer.setForeground(c);
+            }
+        });
+        bouton_fermer.setBounds(260, 310, 123, 41);
+
+
+        //bouton Regle
+        bouton_regle = new JoliBouton("Règle");
+        bouton_regle.createButton();
+        bouton_regle.addMouseListener(new java.awt.event.MouseAdapter() {
+            Image img_normal = ImageIO.read(new File("images/button.png"));
+            Image img_over = ImageIO.read(new File("images/button-over.png"));
+
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                bouton_regle.setIcon(new ImageIcon(img_over));
+                Color c = new Color(0xDA6C0D);
+                bouton_regle.setForeground(c);
+            }
+
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                bouton_regle.setIcon(new ImageIcon(img_normal));
+                Color c = new Color(0x643E1D);
+                bouton_regle.setForeground(c);
+            }
+        });
+        bouton_regle.setBounds(260, 240, 123, 41);
+
+
+        //bouton Jouer
+        bouton_jouer = new JoliBouton("Jouer");
+        bouton_jouer.createButton();
+        bouton_jouer.addMouseListener(new java.awt.event.MouseAdapter() {
+            Image img_normal = ImageIO.read(new File("images/button.png"));
+            Image img_over = ImageIO.read(new File("images/button-over.png"));
+
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                bouton_jouer.setIcon(new ImageIcon(img_over));
+                Color c = new Color(0xDA6C0D);
+                bouton_jouer.setForeground(c);
+            }
+
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                bouton_jouer.setIcon(new ImageIcon(img_normal));
+                Color c = new Color(0x643E1D);
+                bouton_jouer.setForeground(c);
+            }
+        });
+        bouton_jouer.setBounds(260, 170, 123, 41);
+
+        //Label choix des joueurs
+        Image im_lab = ImageIO.read(new File("images/label-background.png"));
+        label_choix_joueur = new JLabel("Choix des Joueurs");
+        label_choix_joueur.setIcon(new ImageIcon(im_lab));
+        label_choix_joueur.setFont(new Font("Matura MT Script Capitals", Font.ROMAN_BASELINE, 20));
+        Color c = new Color(0x643E1D);
+        
+        label_choix_joueur.setHorizontalTextPosition(JLabel.CENTER);
+        label_choix_joueur.setVerticalTextPosition(JLabel.CENTER);
+        label_choix_joueur.setForeground(c);
+        label_choix_joueur.setBounds(130, 145, 400, 40);
+        label_choix_joueur.setVisible(false);
+        //bouton ajout 1er joueur
+        
+        
+
+        //fenetre principale
+        accueil = new Accueil();
+        accueil.setOpaque(true);
+        accueil.setLayout(null);
+
+
+        //ajout des boutons
+        accueil.add(bouton_fermer);
+        accueil.add(bouton_jouer);
+        accueil.add(bouton_regle);
+        accueil.add(label_choix_joueur);
+
+        //ajout des actions
+        bouton_regle.addActionListener(accueil);
+        bouton_jouer.addActionListener(accueil);
+        bouton_fermer.addActionListener(accueil);
+        
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         Object obj = e.getSource();
-        if(obj == bouton_quitter){
-            System.exit(0);
+        String action = String.valueOf(obj);
+        int debut = action.indexOf("text=") + 5;
+        int fin = action.indexOf(",defaultCapable=");
+        String action_name = action.substring(debut, fin);
+        switch (action_name) {
+            case "Fermer":
+                System.exit(0);
+                break;
+            case "Jouer":
+                bouton_jouer.setVisible(false);
+                bouton_fermer.setVisible(false);
+                bouton_regle.setVisible(false);
+                label_choix_joueur.setVisible(true);
+                break;
+            case "Règle":
+                System.exit(0);
+                break;
         }
     }
-    
+
+    @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        int x = (getWidth() - background.getWidth())/2;
-        int y = (getHeight() - background.getHeight())/2;
+        int x = (getWidth() - background.getWidth()) / 2;
+        int y = (getHeight() - background.getHeight()) / 2;
         g.drawImage(background, x, y, this);
     }
-    
 }
--- src/interfaceGraphique/JoliBouton.java
+++ src/interfaceGraphique/JoliBouton.java
@@ -1,12 +1,8 @@
 package interfaceGraphique;
 
 import java.awt.Color;
-import java.awt.Container;
-import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
@@ -16,7 +12,6 @@ import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 
 public class JoliBouton extends JButton implements MouseListener {
 
@@ -27,57 +22,39 @@ public class JoliBouton extends JButton implements MouseListener {
     public JoliBouton(String nom) {
         super();
         bouton = new JButton();
-        bouton.setText(nom);
-
+        this.setText(nom);
+        this.setHorizontalTextPosition(JButton.CENTER);
+        this.setVerticalTextPosition(JButton.CENTER);
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
+    protected void createButton() {
         try {
-            super.paintComponent(g);
             img_normal = ImageIO.read(new File("images/button.png"));
             img_over = ImageIO.read(new File("images/button-over.png"));
-            bouton.setIcon(new ImageIcon(img_normal));
-            bouton.setFont(new Font("Matura MT Script Capitals", Font.ROMAN_BASELINE, 20));
-            bouton.setHorizontalTextPosition(JButton.CENTER);
-            bouton.setVerticalTextPosition(JButton.CENTER);
+            this.setIcon(new ImageIcon(img_normal));
+            this.setFont(new Font("Matura MT Script Capitals", Font.ROMAN_BASELINE, 20));
             Color c = new Color(0x643E1D);
-            bouton.setForeground(c);
-            bouton.setFocusPainted(false);
-            bouton.setBorderPainted(false);
-            bouton.setContentAreaFilled(false);
+            this.setForeground(c);
+            this.setFocusPainted(false);
+            this.setBorderPainted(false);
+            this.setContentAreaFilled(false);       
         } catch (IOException ex) {
             Logger.getLogger(JoliBouton.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     @Override
-    public void mouseEntered(java.awt.event.MouseEvent evt) {
-        bouton.setIcon(new ImageIcon(img_over));
-        Color c = new Color(0xDA6C0D);
-        bouton.setForeground(c);
-    }
+    public void mouseEntered(java.awt.event.MouseEvent evt) {}
 
     @Override
-    public void mouseExited(java.awt.event.MouseEvent evt) {
-        bouton.setIcon(new ImageIcon(img_normal));
-        Color c = new Color(0x643E1D);
-        bouton.setForeground(c);
-    }
-
-    //}
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    public void mouseExited(java.awt.event.MouseEvent evt) {}
 
     @Override
-    public void mousePressed(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+    public void mouseClicked(MouseEvent e){}
+    
     @Override
-    public void mouseReleased(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    public void mousePressed(MouseEvent e) {}
+    
+    @Override
+    public void mouseReleased(MouseEvent e) {}
 }
