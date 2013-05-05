1ccca6d3ddbebb9bd6157b624eee82fbcca1943f
Quentin Burtschell <quentin.burtschell@gmail.com>
2013-05-05 21:26:48 +0200

    mod

--- src/interfaceGraphique/Bouton3Bois.java
+++ src/interfaceGraphique/Bouton3Bois.java
@@ -30,9 +30,9 @@ public class Bouton3Bois extends JButton implements ActionListener{
         Color c = Agricola.getJoueurCourant().getInterfaceColor();
         GradientCircularButton gci = new GradientCircularButton(Agricola.getJoueurCourant().getNom(), c);
         gci.setVisible(true);
-        int x = this.getHorizontalAlignment();
-        int y = this.getVerticalAlignment();
-        gci.setBounds(x+50, y+50, 100, 100);
+        int x = this.getX();
+        int y = this.getY();
+        gci.setBounds(x+50, y+10, 100, 100);
         InterfacePlateau.getFrame().add(gci);
         if(result == 0)
             this.action.action(Agricola.getJoueurCourant());
